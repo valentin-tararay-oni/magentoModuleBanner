ed29862d65639c694d583e2a5b3cabaaed9968ea
valentin tararay <valentin.tararay@onilab.com>
2021-10-15 11:26:43 +0300

    default.xml

--- app/code/TM/BannerModule/view/frontend/web/js/banner_script.js
+++ app/code/TM/BannerModule/view/frontend/web/js/banner_script.js
@@ -1,42 +1,43 @@
+    define(['jquery'], function ($) {
+        return function (config, element) {
 
-define(['jquery'], function($){
-    return function(config, element) {
+            function sendAjax(data) {
+                if (!data) {
+                    data = '';
+                }
+                $.ajax({
+                    url: config.url,
+                    type: "POST",
+                    data: {'oneshow': data},
+                    showLoader: true,
+                    cache: false,
+                    success: function (response) {
+                        $(".banner-block").html(response.output);
+                        if ($(".banner-block img").attr('data-oneshow') !== undefined && $(".banner-block img").attr('data-oneshow') !== false) {
+                            let id = $(".banner-block img").attr('data-id');
 
-        function sendAjax(data) {
-            $.ajax({
-                url: config.url,
-                type: "POST",
-                data: {'oneshow': data},
-                showLoader: true,
-                cache: false,
-                success:  function (response) {
-                    $(".banner-block").html(response.output);
-                    if ($(".banner-block img").attr('data-oneshow') !== undefined && $(".banner-block img").attr('data-oneshow') !== false) {
-                        let id = $(".banner-block img").attr('data-id');
-                        if(localStorage.getItem('oneShow')){
-                            let arId = localStorage.getItem('oneShow');
+                            if (localStorage.getItem('oneShow')) {
+                                let arId = JSON.parse(localStorage.getItem('oneShow'));
+                                if (!arId.includes(id)) {
+                                    arId.push(id);
+                                    localStorage.setItem('oneShow', JSON.stringify(arId));
+                                }
 
-                            if(!arId.includes(id)){
-                                arId.push(id);
-                                 localStorage.setItem('oneShow',JSON.stringify(arId));
+                            } else {
+                                let arId = [];
+                                arId.push(id)
+                                localStorage.setItem('oneShow', JSON.stringify(arId))
                             }
 
-                        }else{
-                            localStorage.setItem('oneShow',JSON.stringify([id]))
                         }
-
                     }
-                }
-            });
-        }
+                });
+            }
 
-
-
-        if(config.action == 'getBanner') {
-            let localStor = localStorage.getItem('oneShow');
-        sendAjax(localStor);
+            if (config.action == 'getBanner') {
+                let localStor = JSON.parse(localStorage.getItem('oneShow'));
+                sendAjax(localStor);
+            }
 
         }
-
-    }
-});
+    });
